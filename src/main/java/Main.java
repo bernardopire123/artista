6418dcf95160bed306f70e7ef8d4263cc74262fb
bernardopire123 <169068301+bernardopire123@users.noreply.github.com>
2024-05-13 14:39:21 +0000

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,31 @@
+// import static org.junit.jupiter.api.Assertions.assertEquals;
+
+// import org.junit.jupiter.api.Test;
+
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("########");
+    System.out.println(" eminem "); 
+    System.out.println("nascimeto:      17 de Outubro de 1972);
+   
+    System.out.println("");
+
+    System.out.println("Seu nome artístico, Eminem, foi criado a partir das iniciais de seu nome Marshall Mathers, ou seja, M&M. Como Eminem não poderia usar esse nome, por conta da marca de chocolates, ele preciso mudar o e resolver escrevê-lo como é falado em inglês: EM-IN-EM.");
+
+    System.out.println("");
+
+    System.out.println("+++++++++");
+    System.out.println()
+                      
+                      
+    
+      
+    )
+                       ")
+  }
+
+  // @Test
+  // void addition() {
+  //     assertEquals(2, 1 + 1);
+  // }
+}
