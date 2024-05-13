821b0856ccd1ad97c2e209b35602c903e511fa61
bernardopire123 <169068301+bernardopire123@users.noreply.github.com>
2024-05-13 15:18:13 +0000

    1305

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,23 +5,35 @@
 public class Main {
   public static void main(String[] args) {
     System.out.println("########");
-    System.out.println(" eminem "); 
-    System.out.println("nascimeto:      17 de Outubro de 1972);
+    System.out.println("##eminem#");
+    System.out.println("#########");
+    System.out.println("nascimeto: 17 de Outubro de 1972");
    
     System.out.println("");
+
+    System.out.println("Álbum");
 
     System.out.println("Seu nome artístico, Eminem, foi criado a partir das iniciais de seu nome Marshall Mathers, ou seja, M&M. Como Eminem não poderia usar esse nome, por conta da marca de chocolates, ele preciso mudar o e resolver escrevê-lo como é falado em inglês: EM-IN-EM.");
 
     System.out.println("");
 
-    System.out.println("+++++++++");
-    System.out.println()
-                      
-                      
-    
+    System.out.println("++++++++++");
+    System.out.println("+++++Músicas++++");
+    System.out.println("kill You 4:24");
+    System.out.println("The Real Slim Shady 4:44");
+    System.out.println("Stan 6:44");
+    System.out.println("Who Knew 3:48");
+    System.out.println("The Way I Am 4:50");
+    System.out.println("Remember Me? 3:39");
+    System.out.println("I Am Back 5:10");
+    System.out.println("Marshall Mathers 5:21");
+    System.out.println("Drug Ballad 5:00");
+    System.out.println("Amityville 4:15");
+    System.out.println("Bitch Please 2 4:48");
+    Sytem.out.println()
       
-    )
-                       ")
+    
+              
   }
 
   // @Test
